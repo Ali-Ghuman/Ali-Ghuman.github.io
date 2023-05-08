042182df315990567d656a0b415f7a7a8244dd1e
Ali-Ghuman <alighuman50@gmail.com>
2023-05-08 01:12:23 -0400

    finished frontend

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,25 @@
-/*!
-* Start Bootstrap - Scrolling Nav v5.0.6 (https://startbootstrap.com/template/scrolling-nav)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-scrolling-nav/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
 
 window.addEventListener('DOMContentLoaded', event => {
 
+
+    // var form1 = document.getElementById("labels-checkbox");
+
+    // document.getElementById("next-btn-labels").addEventListener("click", function () {
+    //     form1.submit();
+    // });
+
+    // var form2 = document.getElementById("newfood-mc");
+
+    // document.getElementById("next-btn-newfoods").addEventListener("click", function () {
+    //     form2.submit();
+    // });
+
+    // var form3 = document.getElementById("labels-checkbox2");
+
+    // document.getElementById("next-btn-labels2").addEventListener("click", function () {
+    //     form3.submit();
+    // });
+    
     // Activate Bootstrap scrollspy on the main nav element
     const mainNav = document.body.querySelector('#mainNav');
     if (mainNav) {
@@ -32,3 +43,21 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+submitForm = function () {
+    document.getElementById("labels-checkbox").submit();
+    document.getElementById("newfood-mc").submit();
+    document.getElementById("labels-checkbox2").submit();
+    document.getElementById("zip-code").submit();
+    document.getElementById("nutrition").submit();
+}
+
+function checkOnlyOne(b) {
+    console.log(b)
+    var x = document.getElementsByClassName('radio');
+    var i;
+
+    for (i = 0; i < x.length; i++) {
+        if (x[i].value != b) x[i].checked = false;
+    }
+}
